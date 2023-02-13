8bad5526fb92f2e03efeefa3b4b179e08be40eba
dgwn <danielgawne@gmail.com>
2023-02-12 21:14:30 -0500

    exercise 9.5 - add endpoint for bmiCalculator using query string params

--- bmi_calculator/bmiCalculator.ts
+++ bmi_calculator/bmiCalculator.ts
@@ -1,9 +1,12 @@
-const height: number = Number(process.argv[2]);
-const weight: number = Number(process.argv[3]);
+// const height: number = Number(process.argv[2]);
+// const weight: number = Number(process.argv[3]);
 
 const calculateBmi = (height: number, weight: number): string => {
+  if (isNaN(height) || isNaN(weight) || !height || !weight) {
+    throw new Error("error: malformatted parameters");
+  }
   const bmi = weight / ((height * height) / 10000);
-  console.log(bmi);
+  // console.log(bmi);
   if (bmi < 18.5) {
     return "Underweight (unhealthy weight)";
   } else if (bmi >= 18.5 && bmi <= 25) {
@@ -14,4 +17,6 @@ const calculateBmi = (height: number, weight: number): string => {
   return "Something went wrong.";
 };
 
-console.log(calculateBmi(height, weight));
+// console.log(calculateBmi(height, weight));
+
+export { calculateBmi };
--- bmi_calculator/index.ts
+++ bmi_calculator/index.ts
@@ -1,8 +1,13 @@
 import express from "express";
+import { calculateBmi } from "./bmiCalculator";
 const app = express();
 
 app.get("/hello", (_req, res) => {
   res.send("Hello Full Stack");
+});
+
+app.get("/bmi", (req, res) => {
+  res.send(calculateBmi(Number(req.query.height), Number(req.query.weight)));
 });
 
 const PORT = 3003;
